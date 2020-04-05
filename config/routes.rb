cc6f44d86c6767c9bd713f45dfed21d838204bb9
Hoxha94 <selimiosman94@gmail.com>
2020-04-05 14:09:50 +0200

    create user parts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
+  get 'contact', to:'static_pages#contact'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200405111559_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.text :city
+      t.text :address
+    end
+  end
+end
